cb7fe6a2b5b0faebab12c9dbeb4305ed4317bffd
Rana Mudassar Rasool <razald4m@gmail.com>
2023-09-29 04:42:07 -0700

    Add files via upload

---
+++ lec 1 use of variable.php
@@ -0,0 +1,38 @@
+<?php 
+// Store a string in a variable and display it:
+
+  $name = "Bard";
+  echo $name;  
+
+
+  // Store an integer in a variable and display it:
+
+  $age = 1;
+echo $age;
+
+// Store a float in a variable and display it:
+
+$price = 10.5;
+echo $price;
+
+
+// sum of 2 number in php 
+
+$num1 = 1;
+$num2 = 2;
+$sum = $num1 + $num2;
+echo $sum;
+
+// To find the average of four numbers in PHP
+$num1 = 1;
+$num2 = 2;
+$num3 = 3;
+$num4 = 4;
+
+$sum = $num1 + $num2 + $num3 + $num4;
+$average = $sum / 4;
+
+echo $average;
+
+
+?>
